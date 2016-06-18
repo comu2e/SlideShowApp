e20d59c732eeac2b63ca23f20b48778622873334
comu2e <dvlpEiji@gmail.com>
2016-06-19 03:47:38 +0900

    スライド画像３枚の追加 拡大画像を表示するクラスEnlargedImageViewControllerの追加

---
+++ SlideShowApp/EnlargedImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EnlargedImageViewController.swift
+//  SlideShowApp
+//
+//  Created by Eiji Takahashi on 2016/06/19.
+//  Copyright © 2016年 devlpEiji. All rights reserved.
+//
+
+import UIKit
+
+class EnlargedImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
