b94420044f1b776b198d87e03237be219f33c1e6
comu2e <dvlpEiji@gmail.com>
2016-06-23 00:06:02 +0900

    next関数，back関数で画像を一つ先送り，巻き戻しする NStimerのtimerインスタンスで２秒に一回next関数を呼び出しするようにする nextButton,BackButtonでnext,back関数をそれぞれお呼び出しできる

--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //画像
+    let images =
+            [
+            UIImage(named:"image1.jpg") as! AnyObject,
+            UIImage(named:"image2.jpg")as! AnyObject,
+            UIImage(named:"image3.jpg")as! AnyObject,
+            ]
+    //スライドショーを表示するslideShowViewer
+    @IBOutlet weak var slideShowViewer: UIImageView!
+    //拡大するボタン
+    @IBAction func enlargeImage(sender: AnyObject) {
+        
     }
-
+    //次の画像に行くボタン，ここでnextを呼ぶ
+    @IBAction func nextButton(sender: AnyObject) {
+    }
+    //前の画像に戻るボタン，ここでbackを呼ぶ
+    @IBAction func backButton(sender: AnyObject) {
+    }
+    //画像を次に進める関数
+    func next(index:Int,image_count:Int) {
+        let index = (index + 1) % image_count
+        slideShowViewer.image = (images[index] as! UIImage)
+        
+    }
+    //画像を前に戻す関数
+    func back(index:Int,image_count:Int){
+        let index = (index - 1)%image_count
+        slideShowViewer.image = (images[index] as! UIImage)
+    }
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        let image_number = images.count - 1
+        let timer:NSTimer
+        timer = NSTimer.scheduledTimerWithTimeInterval(
+            2.0,
+            target: self,
+            selector:(Selector("next(0,image_count:\(image_number))")),
+            userInfo: nil,
+            repeats: true)
+       timer.fire()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
